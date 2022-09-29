fa3736f87fe9174851a6713cd9c01363ee1bee15
AimarBustamante <106636449+AimarBustamante@users.noreply.github.com>
2022-09-29 11:16:50 -0500

    Add files via upload

---
+++ news.js
@@ -0,0 +1,12 @@
+// *Scroll reveal
+
+ScrollReveal().reveal(".news-title");
+ScrollReveal().reveal(".back-button");
+ScrollReveal().reveal(".img-container", {delay: 500});
+ScrollReveal().reveal(".news-facts", {delay: 500});
+ScrollReveal().reveal(".context", {delay: 500});
+ScrollReveal().reveal(".subtitle", {delay: 500});
+ScrollReveal().reveal(".up-button", {delay: 500});
+ScrollReveal().reveal(".social", {delay: 500});
+ScrollReveal().reveal(".footer-links", {delay: 500});
+
